60159ebe29aada7fd04af46d39578533af9eec27
Ian Andres Fack Cardenas <ianandres.fack@alumnos.ulagos.cl>
2025-05-28 15:26:38 -0400

    Tuplas en python

---
+++ 07-tuplas.py
@@ -0,0 +1,15 @@
+#Tupla de estudiantes
+estudiantes = ("Samir", "Ivan", "Ian","Antonio")
+print  (estudiantes)
+
+#estudiantes.pop() esto no se puede porque la tupla no se puede modificar
+#print(estudiantes)
+#estudiantes = ("Pablo")
+
+#Index indica la posicion del elemento
+print(estudiantes.index ("Samir"))
+print (estudiantes)
+
+#Sorted ordena elementos en una tupla
+print(sorted(estudiantes)) #Y tambien lo transforma en una lista
+
