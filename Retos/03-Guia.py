0ed907ce39d36eb7753bbefc43c38a030e761cc6
Ian Andres Fack Cardenas <ianandres.fack@alumnos.ulagos.cl>
2025-06-02 12:59:16 -0400

    Un cambio

--- Retos/03-Guia.py
+++ Retos/03-Guia.py
@@ -5,4 +5,7 @@ print (Palabra_en_mayuscula)
 Palabra_en_minuscula = Palabra.lower()
 print (Palabra_en_minuscula)
 
-print (Palabra.count ["A","a"])
+print ("Esta es la cantidad de letras a en mayuscula:", Palabra.count ("A"))
+print ("Esta es la cantidad de letras a en minuscula:", Palabra.count ("a"))
+
+print ("Esta es la la longitud de tu palabra hecha" , len(Palabra))
