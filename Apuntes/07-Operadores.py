79ea0f36c2f6d3f051d1408fc20258221a5245ba
Ian Andres Fack Cardenas <ianandres.fack@alumnos.ulagos.cl>
2025-06-03 13:03:06 -0400

    Operadores

---
+++ Apuntes/07-Operadores.py
@@ -0,0 +1,23 @@
+t = 5.39 
+g = 9.81
+
+v = g * t 
+
+c1 = 4+3j
+print (type(c1))
+
+c2 = complex (3.-5)
+print("El numero complejo es:", c2)
+print (f"La velocidad del objeto en caida libre es de: {v:.2f}")
+
+print (c2.real)
+
+animal_domestico = "Gato"
+animal_salvaje = "Loro"
+
+# == igual a
+# != desigual a
+# > mayor que 
+# < menor que 
+# >= mayor o igual que 
+# <= menor o igual que 
