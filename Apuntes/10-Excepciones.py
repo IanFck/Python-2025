afa361152f0598322c75a355a8a5dd5545a66ce1
Ian Andres Fack Cardenas <ianandres.fack@alumnos.ulagos.cl>
2025-06-10 13:09:38 -0400

    Excepciones en python

---
+++ Apuntes/10-Excepciones.py
@@ -0,0 +1,14 @@
+entrada = input("Ingrese un valor entero:")
+
+#ES como correcion de errores, es una forma de corregir errores
+try:
+    numero = int(entrada)
+    print ("Numero ingresado:", entrada)
+except ValueError: #Error por tipo de dato
+    print("Error de valor: el valor ingresado no es entero")
+except ValueError as e:
+    print(f"Error inesperado: {e}")#Errores genericos e inesperados
+else:
+    print("Conversion fue existosa!")
+finally:
+    print("Finalizacion del bloque")
