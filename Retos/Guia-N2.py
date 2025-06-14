9e46172dea7a2fafd8aa785ff166d0f2d8786ec8
Ian Andres Fack Cardenas <ianandres.fack@alumnos.ulagos.cl>
2025-06-13 21:39:16 -0400

    Intentando resolver esta pinche guia :(

--- Retos/Guia-N2.py
+++ Retos/Guia-N2.py
@@ -8,8 +8,13 @@ Palabra = input("Ingrese el parrafo :")
 Lista = (list(Palabra.split())) #Aqui separaremos la lista con todas las palabras ingresadas del parrafo
 print (Lista)
 Palabra_a_buscar = input("Dime una palabra a buscar") #Aqui le pediremos al usuario una palabra a buscar
-buscar = Lista.count(Palabra_a_buscar)
-print(buscar)   
+try: 
+    buscar = Palabra.count(Palabra_a_buscar.lower())
+    print(buscar) 
+except ValueError:
+    print ("“El texto no puede estar vacío")  
+finally:
+    print ("Fin del proceso")
 
 #Ejercicio 2
 print ("---------------Ejercicio 2----------------")
@@ -20,13 +25,44 @@ S = 500
 
 
 #Ejercicio 3
-print ("---------------Ejercicio 2----------------")
+print ("---------------Ejercicio 3----------------")
 Vendedores = {
             "Juan": [100000, 120000, 250000, 80000, 160000],
             "Alvaro":[200000, 60000, 250000, 140000, 90000],
             "Pedro" : [300000, 79000, 140000, 210000, 70000],
 }
 
-Ventas_Juan = (Vendedores["Juan" ])
-ventas_Alvaro = (Vendedores["Alvaro"])
-ventas_Pedro = (Vendedores["Pedro"])
+Ventas_Juan = list((Vendedores["Juan" ]))
+ventas_Alvaro = list((Vendedores["Alvaro"]))
+ventas_Pedro = list((Vendedores["Pedro"])) 
+
+sueldo_base = 529000
+
+Ventas_semanales_Juan = sum(Ventas_Juan) / len(Ventas_Juan)
+Ventas_semanales_Alvaro = sum(ventas_Alvaro) / len(ventas_Alvaro)
+Ventas_semanales_Pedro = sum(ventas_Pedro) / len(ventas_Pedro)
+
+
+print (Ventas_semanales_Juan)
+#Ejercicio 4
+print ("---------Ejercicio 4-------")
+
+Numero = input("Dame un numero")
+impar = 1
+
+#Ejercicio 5
+print ("-----------Ejercicio 5--------------")
+
+Tablero = {
+            ""
+
+
+}
+
+#Ejercicio 6
+print ("-----------Ejercicio 6---------------")
+for hora in range(24):
+    for minuto in range(60):
+        for segundo in range(60):
+            print(f"{hora:02d}:{minuto:02d}:{segundo:02d}")
+
