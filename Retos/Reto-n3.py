b292da4ea84a0e226ad9712e79cd446893a2fdfc
Ian Andres Fack Cardenas <ianandres.fack@alumnos.ulagos.cl>
2025-06-16 16:31:32 -0400

    Reto n3

--- Retos/Reto-n3.py
+++ Retos/Reto-n3.py
@@ -1,10 +1,14 @@
-num = 500
-while num <= 100:
+numeros = list(range(500,99,-3)) #Aqui haremos una lista con un rango entre 500 a 99 y se incrementa por 3
+
+print("Numeros del 500 al 100 incrementado de 3 en 3")
+for num in numeros: #Aqui haremos un bucle donde num sera una variable que pasara por numeros y asi hasta llegar al limite
     print (num)
-    num -= 3
 
-#2do intento
-for i in range(100,500,3):
-    print(i)
+suma = sum(numeros)
+print(suma)
 
-#La verdad no supe como hacerlo de 500 para abajo :(
+promedio = suma / len(numeros)
+print (promedio)
+
+print(f"Suma total: {suma}")
+print(f"Promedio total: {promedio:.2f}")
