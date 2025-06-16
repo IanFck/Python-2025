6dafa5a1d671c162006a60b8d8e3f963829a964a
Ian Andres Fack Cardenas <ianandres.fack@alumnos.ulagos.cl>
2025-06-15 20:57:00 -0400

    Trabajos

---
+++ Archisalud/Codigo-ArchiSalud-main/Codigo-ArchiSalud-main/archisalud_login_actualizado/script.js
@@ -0,0 +1,5 @@
+
+function redirigir(event) {
+  event.preventDefault();
+  window.location.href = "https://www.google.com/search?gs_ssp=eJzj4tTP1TcwMU02T1JgNGB0YPBiS8_PT89JBQBASQXT&q=google&rlz=1C1ONGR_esCL991CL991&oq=g&gs_lcrp=EgZjaHJvbWUqEwgBEC4YgwEYxwEYsQMY0QMYgAQyBggAEEUYPDITCAEQLhiDARjHARixAxjRAxiABDIGCAIQRRg5MgYIAxBFGDsyBggEEEUYPDIGCAUQRRg9MgYIBhBFGDwyBggHEEUYQdIBCDI2NjhqMGo3qAIIsAIB8QU_eYDLsi5eqw&sourceid=chrome&ie=UTF-8";
+}
