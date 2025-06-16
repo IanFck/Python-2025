c9a0cdf31f48257620fc0da378d2a4ae830e54ea
Ian Andres Fack Cardenas <ianandres.fack@alumnos.ulagos.cl>
2025-06-15 21:11:24 -0400

    Modifiqué 04-tuplas.py

--- Apuntes/04-tuplas.py
+++ Apuntes/04-tuplas.py
@@ -2,6 +2,18 @@
 estudiantes = ("Samir", "Ivan", "Ian","Antonio")
 print  (estudiantes)
 
+print ("Ahora veremos las tuplas")
+#Las tuplas no se pueden modificar y eliminar
+#CREANDO UNA TUPLA
+tupla = tuple ()
+estudiantes2 = ("Samir", "Ivan", "Ian")
+frutas = []
+print (estudiantes2)
+print (type (tupla))
+print (type (frutas))
+
+print (estudiantes2.index ("Ivan"))
+
 #estudiantes.pop() esto no se puede porque la tupla no se puede modificar
 #print(estudiantes)
 #estudiantes = ("Pablo")
