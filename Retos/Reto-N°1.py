2f3bd2d61f14a3ffdadf202b1eefbf5a25b3e17c
Ian Andres Fack Cardenas <ianandres.fack@alumnos.ulagos.cl>
2025-06-23 21:15:41 -0400

    Reto N°1 en Python

---
+++ Retos/Reto-N°1.py
@@ -0,0 +1,6 @@
+print ("Hola te pedire un algoritmo para calcular tu edad, ademas necesito que me des tu nombre y tu edad")
+nombre = input ("Indicame tu nombre")
+edad = int (input ("Ingresa una edad"))
+total_edad = (edad + 20)
+
+print ("Hola mi nombre es", nombre, "tengo" ,edad,"años y en 20 años tendre" ,total_edad, " años")
