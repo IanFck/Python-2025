c4e4a3f979f34be9872df157b6d1a0e02e709fd2
Ian Andres Fack Cardenas <ianandres.fack@alumnos.ulagos.cl>
2025-06-23 21:14:06 -0400

    Reto N°2 en Python

--- Retos/Reto-N°2.py
+++ Retos/Reto-N°2.py
@@ -4,17 +4,15 @@ inventario = {
         "Cereza": (600,1200,1500)
 }
 
+precios_platano = inventario["Platano"]
+precios_nuevos_platanos = list(set(precios_platano)) #Lo cambiamos a set porque no permite duplicados y luego lo transformamos en una lista para que tenga orden
+
+tipos_frutas = list(inventario.keys()) #Creamos con list para transformar la tupla en una lista porque es lo que nos pidieron y usamos .keys para llevarnos las llaves de el diccionario
+
+promedio_platano = sum(precios_nuevos_platanos) / len(precios_nuevos_platanos) #Para sacar un promedio siempre usamos Len para que nos divida
+
 print(inventario)
-
-
-
-tipos_frutas = inventario
-tipos_frutas.keys
+print(precios_nuevos_platanos)
 print(tipos_frutas)
+print("Promedio de precios únicos del plátano:", "$", promedio_platano) 
 
-
-precios_platano = (inventario["Platano"])
-print(precios_platano)
-
-promedio_platano = (inventario["Platano"],round)
-print(promedio_platano)
