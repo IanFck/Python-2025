9e8b4b9706980de979d3fc06e17b8bd0dbdf0357
Ian Andres Fack Cardenas <ianandres.fack@alumnos.ulagos.cl>
2025-06-30 19:48:36 -0400

    Archivos Json en Python

---
+++ Apuntes/12-Json.py
@@ -0,0 +1,24 @@
+import json #Manipulacion archivos Json
+import os #Metodos para trabajar con el sistema operativo
+
+ruta = os.path.join("json", "datos.json") #Ruta relativa /json/datos.json
+
+#Leer el archivo Json
+
+with open(ruta, "r", encoding="utf-8") as archivo:
+    datos = json.load(archivo)
+
+print("\nTodos los usuarios:")
+for usuario in datos:
+    print(f"ID: {usuario["id"]}, Nombre: {usuario["nombre"]}, Edad:{usuario["edad"]}")
+
+nuevo_usuario = {
+    "id":5,
+    "nombre": "Fernanda",
+    "Edad": 30
+}
+
+datos.append(nuevo_usuario)
+
+with open(ruta, "r", encoding="utf-8") as archivo:
+    
